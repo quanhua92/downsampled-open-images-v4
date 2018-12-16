d1572391cfc7bafdf67d77ffb26d03c7320b812c
Quan Hua <quanhua92@gmail.com>
2018-12-16 13:13:16 +0700

    complete code to resize images

--- README.md
+++ README.md
@@ -3,14 +3,53 @@ Downsampled Open Images Dataset V4
 
 ## Introduction
 The [Open Images V4 dataset](https://ai.googleblog.com/2018/04/announcing-open-images-v4-and-eccv-2018.html) contains 15.4M bounding-boxes for 600 categories on 1.9M images and 30.1M human-verified image-level labels for 19794 categories. 
-The dataset is available at this (link)[https://storage.googleapis.com/openimages/web/download.html]. This total size of the full dataset is `18TB`. There's also a smaller version which contains rescaled images to have at most 1024 pixels on the longest side. However, the total size of the rescaled dataset is still large (`513GB` for training, `12GB` for validation and `36GB` for testing).
+The dataset is available at this [link](https://storage.googleapis.com/openimages/web/download.html). This total size of the full dataset is `18TB`. There's also a smaller version which contains rescaled images to have at most 1024 pixels on the longest side. However, the total size of the rescaled dataset is still large (`513GB` for training, `12GB` for validation and `36GB` for testing).
 
-I provide a much smaller version of the Open Images Dataset V4, as inspired by `Downsampled ImageNet datasets` [@PatrykChrabaszcz](https://github.com/PatrykChrabaszcz/Imagenet32_Scripts).
+I provide a much smaller version of the Open Images Dataset V4, as inspired by `Downsampled ImageNet datasets` [@PatrykChrabaszcz](https://github.com/PatrykChrabaszcz/Imagenet32_Scripts). These downsampled dataset are much smaller in size so everyone can download it with ease. Experiments on these downsampled dataset are also much faster than the original.
+
+## Data
+- Open Images V4 - 512 px version.
+
+## Requirements
+- pillow
+- loguru
+
+## Usage
+```
+image_resizer.py [-h] -i IN_DIR -o OUT_DIR [-s SIZE] [-ext EXTENSION]
+                        [-a ALGORITHM] [-j PROCESSES] [-l LOG]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i IN_DIR, --in_dir IN_DIR
+                        Input directory with source images
+  -o OUT_DIR, --out_dir OUT_DIR
+                        Output directory for resized images
+  -s SIZE, --size SIZE  Size of an output image (e.g. 512 results in (512x512)
+                        image)
+  -ext EXTENSION, --extension EXTENSION
+                        Extension of the output image (jpg, png). Default
+                        empty means the same as source
+  -a ALGORITHM, --algorithm ALGORITHM
+                        Algorithm used for resampling: lanczos, nearest,
+                        bilinear, bicubic, box, hamming
+  -j PROCESSES, --processes PROCESSES
+                        Number of sub-processes that run different folders in
+                        the same time
+  -l LOG, --log LOG     Path of the output log
+```
+
+For example, the 512px dataset is created with the following command:
+
+```
+python image_resizer.py -i data\train -o output\train -j 4 -ext jpg -a lanczos
+```
 
 
 ## References
 
-- [Open Images V4 dataset](https://ai.googleblog.com/2018/04/announcing-open-images-v4-and-eccv-2018.html)
+- [Open Images V4 dataset](https://storage.googleapis.com/openimages/web/download.html)
+- [Announcing Open Images V4 and the ECCV 2018 Open Images Challenge](https://ai.googleblog.com/2018/04/announcing-open-images-v4-and-eccv-2018.html)
 
 ## Acknowledgements ##
 
--- image_resizer.py
+++ image_resizer.py
@@ -21,6 +21,7 @@ def parse_arguments():
     parser.add_argument('-o', '--out_dir', help="Output directory for resized images", required=True)
     parser.add_argument('-s', '--size', help="Size of an output image (e.g. 512 results in (512x512) image)",
                         default=512, type=int)
+    parser.add_argument('-ext', '--extension', help="Extension of the output image (jpg, png). Default empty means the same as source", default="")
     parser.add_argument('-a', '--algorithm', help="Algorithm used for resampling: lanczos, nearest,"
                                                   " bilinear, bicubic, box, hamming",
                         default='lanczos')
@@ -30,21 +31,92 @@ def parse_arguments():
                         default=2, type=int)
     parser.add_argument('-l', '--log', help="Path of the output log", default="output.log")
 
+
     args = parser.parse_args()
 
-    return args.in_dir, args.out_dir, args.algorithm, args.size, args.processes, \
+    return args.in_dir, args.out_dir, args.algorithm, args.size, args.extension, args.processes, \
            args.log
 
 @logger.catch
+def resize_images(idx, filenames, in_dir, out_dir, algo, size, extension, log_path):
+    logger.add(log_path, enqueue=True)
+
+    logger.info("Thread {} starts!", idx)
+
+    count_error = 0
+    for filename in filenames:
+        in_path = os.path.join(in_dir, filename)
+        if len(extension) > 0:
+            name, _ = os.path.splitext(filename)
+            out_path = os.path.join(out_dir, name + "." + extension)
+        else:
+            out_path = os.path.join(out_dir, filename)
+
+        # Exception raised when file is not an image
+        try:
+            im = Image.open(in_path)
+
+            # Convert grayscale images into 3 channels
+            if im.mode != "RGB":
+                im = im.convert(mode="RGB")
+
+            # Calculate the output size
+            w, h = im.size
+
+            if w > h:
+                ratio = size * 1.0 / w
+                new_w, new_h = size, int(h * ratio)
+            else:
+                ratio = size * 1.0 / h
+                new_w, new_h = int(w * ratio), size
+
+            # Resize the image
+            im_resized = im.resize((new_w, new_h), algo)
+
+            # Save the image
+            im_resized.save(out_path)
+        except OSError:
+            logger.error("Can not read file name : {}", filename)
+            count_error += 1
+
+    logger.info("Thread {} ends with # err = {}!", idx, count_error)
+
+@logger.catch
 def main():
-    in_dir, out_dir, algorithm, size, processes, log_path = parse_arguments()
+    in_dir, out_dir, algorithm, size, extension, processes, log_path = parse_arguments()
 
     logger.add(log_path, enqueue=True)
 
     logger.info("Begin!")
-    logger.info("in_dir: {}, out_dir: {}, algorithm: {}, size: {}, processes: {}, log_path: {}",
-                    in_dir, out_dir, algorithm, size, processes, log_path)
+    logger.info("in_dir: {}, out_dir: {}, algorithm: {}, size: {}, extension: {}, processes: {}, log_path: {}",
+                    in_dir, out_dir, algorithm, size, extension, processes, log_path)
 
+    pool = Pool(processes=processes)
+
+    # Make sure out_dir
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # Get list image paths
+    list_paths = os.listdir(in_dir)
+
+    logger.info("Total number of file paths are {}", len(list_paths))
+    # Split list into {processes} lists
+    n = len(list_paths) // processes
+    remaining = len(list_paths) - n * processes
+
+    chunks = [list_paths[i : i + n] for i in range(0, len(list_paths) - remaining, n)]
+    chunks[-1] += list_paths[-remaining:]
+
+    # Send each list to threads
+    for idx, chunk in enumerate(chunks):
+        logger.info("Send chunk of size {} to thread {}", len(chunk), idx)
+        pool.apply_async(func=resize_images,
+                        args=[idx, chunk, in_dir, out_dir, alg_dict[algorithm], size, extension, log_path])
+
+    pool.close()
+    pool.join()
+    logger.info("Finish!")
 
 if __name__ == "__main__":
     main()
